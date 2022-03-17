5f8ad6caf39470f853489d3c33ca75ba3de74467
Kuroshiiron <73466349+Kuroshiiron@users.noreply.github.com>
2022-03-18 00:33:03 +0800

    Functions of converting to and uploading PDF

---
+++ index.js
@@ -0,0 +1,37 @@
+// Saves the form value in PDF
+function createPDF() {
+    if (document.getElementById("name").value == "" && document.getElementById("hp").value == "") {
+        alert("Please enter all the fields");
+    }
+    else {
+        // create the jspdf document
+
+        var doc = new jsPDF();
+
+        doc.text(document.getElementById("name").value, 10, 10);
+        doc.text(document.getElementById("hp").value, 25, 25);
+
+        // save the file
+        doc.save("ouput.pdf");
+    }
+}
+
+// function to uplod pdf file and display it in HTML
+const inpFile = document.getElementById("inpFile");
+const btnUpload = document.getElementById("btnUpload");
+const resultText = document.getElementById("resultText");
+
+btnUpload.addEventListener("click", () => {
+    const formData = new FormData();
+
+    formData.append("pdfFile", inpFile.files[0]);
+
+    fetch("/extract-text", {
+        method: "post",
+        body: formData
+    }).then(response => {
+        return response.text();
+    }).then(extractedText => {
+        resultText.value = extractedText.trim();
+    });
+});
